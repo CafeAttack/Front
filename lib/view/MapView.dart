915823cead809784eb016e094d11a30bcfa8bf1e
Eunjin Oh <ibsuny3214@naver.com>
2024-08-15 02:44:12 +0900

    DESIGN: Map Page - 현재위치 버튼 design 완료 (#12)

--- lib/view/MapView.dart
+++ lib/view/MapView.dart
@@ -1,4 +1,5 @@
 import 'package:cafe_attack/view/floatingButton.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:kakao_map_plugin/kakao_map_plugin.dart';
 
@@ -7,44 +8,64 @@ class MapPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
-      floatingActionButton: LabelChange(),
+      floatingActionButton: Stack(children: [
+        Align(
+          alignment:
+              Alignment(Alignment.bottomRight.x - 0.2, Alignment.bottomRight.y),
+          child: FloatingActionButton(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(50),
+            ), //모서리
+            onPressed: () {},
+            backgroundColor: Colors.white,
+            child: Icon(Icons.my_location_rounded),
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomLeft,
+          child: LabelChange(),
+        )
+      ]),
       body: Stack(
         children: [
           KakaoMap(
-            // onMapCreated: (controller) {
-            //   // Handle map creation
-            // },
+              // onMapCreated: (controller) {
+              //   // Handle map creation
+              // },
 
-          ),
-
+              ),
           Positioned(
             top: 50,
             left: 20,
             right: 20,
-            child:
-              Row(
-                children: [
-                  IconButton(onPressed: (){}, icon: Icon(Icons.menu),iconSize: 40,),
-                  SizedBox(width: 10),
-                  Expanded(
-                    child: TextFormField(
-                      decoration: InputDecoration(
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.menu),
+                  iconSize: 40,
+                ),
+                SizedBox(width: 10),
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(
                         filled: true,
                         fillColor: Colors.white,
                         hintText: '카페를 검색하세요...',
-                          enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide.none,
-                          )
-                      ),
-                    ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none,
+                        )),
                   ),
-                  IconButton(onPressed: (){}, icon: Icon(Icons.search),iconSize: 40,)
-                ],
-              ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.search),
+                  iconSize: 40,
+                )
+              ],
             ),
-
-
+          ),
         ],
       ),
     );
