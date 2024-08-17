491babff87d13327cb87eb937d53941ba419a249
Eunjin Oh <ibsuny3214@naver.com>
2024-08-17 11:33:53 +0900

    DESIGN: Cafe Page - 리뷰란 제작 (#19)

--- lib/view/CafeView.dart
+++ lib/view/CafeView.dart
@@ -1,6 +1,7 @@
 import 'package:cafe_attack/MetaData.dart';
 import 'package:cafe_attack/view/resposive/BreakPoint.dart';
 import 'package:cafe_attack/view/resposive/ResponsiveCenter.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CafePage extends StatefulWidget {
@@ -36,125 +37,232 @@ class _CafePageState extends State<CafePage> {
       ),
       body: SingleChildScrollView(
         child: ResponsiveCenter(
-          child: Column(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border(
-                    top: BorderSide(
-                      color: Colors.black,
-                      width: 0.5,
-                    ),
-                    bottom: BorderSide(
-                      color: Colors.black,
-                      width: 0.5,
-                    ),
+          child: Column(children: [
+            Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    color: Colors.black,
+                    width: 0.5,
+                  ),
+                  bottom: BorderSide(
+                    color: Colors.black,
+                    width: 0.5,
                   ),
                 ),
-                padding: EdgeInsets.only(right: 10, left: 10, top: 8,bottom: 8),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          child: Text("위치", style: TextStyle(
+              ),
+              padding: EdgeInsets.only(right: 10, left: 10, top: 8, bottom: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        child: Text(
+                          "위치",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontFamily: freesentation,
+                              fontWeight: FontWeight.w500),
+                          textAlign: TextAlign.center,
+                        ),
+                        width: 80,
+                        height: 23,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      SizedBox(
+                        child: Text(
+                          "서울 노원구 광운로 25 2층",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w300,
+                              fontFamily: freesentation,
+                              fontSize: 20),
+                        ),
+                        height: 23,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        child: Text(
+                          "영업시간",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontFamily: freesentation,
+                              fontWeight: FontWeight.w500),
+                          textAlign: TextAlign.center,
+                        ),
+                        width: 80,
+                        height: 23,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      SizedBox(
+                        child: Text(
+                          "매일 11:00 ~ 23:00 영업",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w300,
+                              fontFamily: freesentation,
+                              fontSize: 20),
+                        ),
+                        height: 23,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        child: Text(
+                          "전화번호",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontFamily: freesentation,
+                              fontWeight: FontWeight.w500),
+                          textAlign: TextAlign.center,
+                        ),
+                        width: 80,
+                        height: 23,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      SizedBox(
+                        child: Text(
+                          "02-6953-0749",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w300,
+                              fontFamily: freesentation,
+                              fontSize: 20),
+                        ),
+                        height: 23,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        child: Text(
+                          "카페유형",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontFamily: freesentation,
+                              fontWeight: FontWeight.w500),
+                          textAlign: TextAlign.center,
+                        ),
+                        width: 80,
+                        height: 23,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      SizedBox(
+                        child: Text(
+                          "프랜차이즈 / 테이크 아웃",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w300,
+                              fontFamily: freesentation,
+                              fontSize: 20),
+                        ),
+                        height: 23,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 20, left: 20, top: 8, bottom: 8),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "사용자 리뷰",
+                        style: TextStyle(
                             fontSize: 20,
                             fontFamily: freesentation,
-                            fontWeight: FontWeight.w500
-                          ),textAlign: TextAlign.center,),
-                          width: 80,
-                          height: 23,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(
+                        width: 3,
+                      ),
+                      Text(
+                        "15건",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w300,
+                          fontFamily: freesentation,
+                          fontSize: 12,
                         ),
-                        SizedBox(width: 5,),
-                        SizedBox(
-                          child: Text("서울 노원구 광운로 25 2층",style: TextStyle(
-                            fontWeight: FontWeight.w300,
-                            fontFamily: freesentation,
-                            fontSize: 20
-                          ),),
-                          height: 23,
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 5,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          child: Text("영업시간", style: TextStyle(
-                              fontSize: 20,
-                              fontFamily: freesentation,
-                              fontWeight: FontWeight.w500
-                          ),textAlign: TextAlign.center,),
-                          width: 80,
-                          height: 23,
-                        ),
-                        SizedBox(width: 5,),
-                        SizedBox(
-                          child: Text("매일 11:00 ~ 23:00 영업",style: TextStyle(
+                      ),
+                      SizedBox(width: 8,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          ),
+                          Icon(
+                            Icons.star_half,
+                            color: Colors.yellow,
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            "4.6/5.0",
+                            style: TextStyle(
                               fontWeight: FontWeight.w300,
                               fontFamily: freesentation,
-                              fontSize: 20
-                          ),),
-                          height: 23,
-                        ),
-                      ],
+                              fontSize: 13,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(width: 10,),
+                  Scrollbar(
+                    child: SizedBox(
+                      height: BreakPoint.tablet, // 원하는 높이 설정
+                      child: ListView(
+                        children: [
+                          Text("data", style: TextStyle(fontSize: 30),),
+                        ],
+                      ),
                     ),
-                    SizedBox(height: 5,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          child: Text("전화번호", style: TextStyle(
-                              fontSize: 20,
-                              fontFamily: freesentation,
-                              fontWeight: FontWeight.w500
-                          ),textAlign: TextAlign.center,),
-                          width: 80,
-                          height: 23,
-                        ),
-                        SizedBox(width: 5,),
-                        SizedBox(
-                          child: Text("02-6953-0749",style: TextStyle(
-                              fontWeight: FontWeight.w300,
-                              fontFamily: freesentation,
-                              fontSize: 20
-                          ),),
-                          height: 23,
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 5,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          child: Text("카페유형", style: TextStyle(
-                              fontSize: 20,
-                              fontFamily: freesentation,
-                              fontWeight: FontWeight.w500
-                          ),textAlign: TextAlign.center,),
-                          width: 80,
-                          height: 23,
-                        ),
-                        SizedBox(width: 5,),
-                        SizedBox(
-                          child: Text("프랜차이즈 / 테이크 아웃",style: TextStyle(
-                              fontWeight: FontWeight.w300,
-                              fontFamily: freesentation,
-                              fontSize: 20
-                          ),),
-                          height: 23,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                  )
+                ],
               ),
-            ],
-          ),
+            ),
+          ]),
           padding: EdgeInsets.zero,
           maxContentWidth: BreakPoint.tablet,
         ),
