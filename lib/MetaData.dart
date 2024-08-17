739707b018f1b6887c656fa9282b12b4c8c932bc
Eunjin Oh <ibsuny3214@naver.com>
2024-08-17 11:18:22 +0900

    DESIGN: Cafe Page - App bar, 카페정보란 제작 (#19)

--- lib/MetaData.dart
+++ lib/MetaData.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 String freesentation = 'Freesentation';
 String mapMaker_unclicked = 'https://i.ibb.co/WpvynM0/1.png';
 String mapMaker_clicked = 'https://i.ibb.co/j5b0gpj/image.png';
+String basic_image = 'assets/Images/temporary_logo.png';
---
+++ lib/view/CafeView.dart
@@ -0,0 +1,164 @@
+import 'package:cafe_attack/MetaData.dart';
+import 'package:cafe_attack/view/resposive/BreakPoint.dart';
+import 'package:cafe_attack/view/resposive/ResponsiveCenter.dart';
+import 'package:flutter/material.dart';
+
+class CafePage extends StatefulWidget {
+  const CafePage({super.key});
+
+  @override
+  State<CafePage> createState() => _CafePageState();
+}
+
+class _CafePageState extends State<CafePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.expand_more),
+        ),
+        title: Text(
+          "설빙 광운대점",
+          style: TextStyle(
+            fontWeight: FontWeight.w400,
+            fontFamily: freesentation,
+            fontSize: 24,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite_border),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: ResponsiveCenter(
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    top: BorderSide(
+                      color: Colors.black,
+                      width: 0.5,
+                    ),
+                    bottom: BorderSide(
+                      color: Colors.black,
+                      width: 0.5,
+                    ),
+                  ),
+                ),
+                padding: EdgeInsets.only(right: 10, left: 10, top: 8,bottom: 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          child: Text("위치", style: TextStyle(
+                            fontSize: 20,
+                            fontFamily: freesentation,
+                            fontWeight: FontWeight.w500
+                          ),textAlign: TextAlign.center,),
+                          width: 80,
+                          height: 23,
+                        ),
+                        SizedBox(width: 5,),
+                        SizedBox(
+                          child: Text("서울 노원구 광운로 25 2층",style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontFamily: freesentation,
+                            fontSize: 20
+                          ),),
+                          height: 23,
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 5,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          child: Text("영업시간", style: TextStyle(
+                              fontSize: 20,
+                              fontFamily: freesentation,
+                              fontWeight: FontWeight.w500
+                          ),textAlign: TextAlign.center,),
+                          width: 80,
+                          height: 23,
+                        ),
+                        SizedBox(width: 5,),
+                        SizedBox(
+                          child: Text("매일 11:00 ~ 23:00 영업",style: TextStyle(
+                              fontWeight: FontWeight.w300,
+                              fontFamily: freesentation,
+                              fontSize: 20
+                          ),),
+                          height: 23,
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 5,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          child: Text("전화번호", style: TextStyle(
+                              fontSize: 20,
+                              fontFamily: freesentation,
+                              fontWeight: FontWeight.w500
+                          ),textAlign: TextAlign.center,),
+                          width: 80,
+                          height: 23,
+                        ),
+                        SizedBox(width: 5,),
+                        SizedBox(
+                          child: Text("02-6953-0749",style: TextStyle(
+                              fontWeight: FontWeight.w300,
+                              fontFamily: freesentation,
+                              fontSize: 20
+                          ),),
+                          height: 23,
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 5,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          child: Text("카페유형", style: TextStyle(
+                              fontSize: 20,
+                              fontFamily: freesentation,
+                              fontWeight: FontWeight.w500
+                          ),textAlign: TextAlign.center,),
+                          width: 80,
+                          height: 23,
+                        ),
+                        SizedBox(width: 5,),
+                        SizedBox(
+                          child: Text("프랜차이즈 / 테이크 아웃",style: TextStyle(
+                              fontWeight: FontWeight.w300,
+                              fontFamily: freesentation,
+                              fontSize: 20
+                          ),),
+                          height: 23,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          padding: EdgeInsets.zero,
+          maxContentWidth: BreakPoint.tablet,
+        ),
+      ),
+    );
+  }
+}
