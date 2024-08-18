f3408a7eb8c21fe8c72949fddce8066f072d209c
Eunjin Oh <ibsuny3214@naver.com>
2024-08-18 22:07:21 +0900

    FIX: Bookmark Page - metadata 수정 (#23)

--- lib/MetaData.dart
+++ lib/MetaData.dart
@@ -5,4 +5,9 @@ String mapMaker_unclicked = 'https://i.ibb.co/WpvynM0/1.png';
 String mapMaker_clicked = 'https://i.ibb.co/j5b0gpj/image.png';
 String basic_image = 'assets/Images/temporary_logo.png';
 
-List<String> categoryId
+List<String> categoryId = [
+  "assets/Images/categoryId_1.png",
+  "assets/Images/categoryId_2.png",
+  "assets/Images/categoryId_3.png",
+  "assets/Images/categoryId_4.png"
+];
