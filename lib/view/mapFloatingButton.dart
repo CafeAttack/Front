c59f9a7626e73fbeba719361e68eab6696a28828
Eunjin Oh <ibsuny3214@naver.com>
2024-08-30 17:15:01 +0900

    REFACTOR: Map Page - 카테고리 Floating 버튼 (#12)

--- lib/view/mapFloatingButton.dart
+++ lib/view/mapFloatingButton.dart
@@ -18,7 +18,46 @@ class _LabelChangeState extends State<LabelChange> {
     });
   }
 
+  SpeedDialChild MapFloatingChild(String _text, Color _color){
+    return SpeedDialChild(
+      labelWidget: SizedBox(
+        width: 80,
+        height: 40,
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 8.0),
+          alignment: Alignment.center,
+          child: Text(
+            _text,
+            style: TextStyle(
+                fontFamily: 'Freesentation',
+                fontWeight: FontWeight.w600,
+                color: _color,
+                fontSize: 14.0),
+          ),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              color: Colors.white),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      onTap: () {
+        onLabelChange(_text);
+        print("Click $_text");
+      },
+    );
+  }
+
   Widget floatingButtons() {
+    var buttons = [
+      MapFloatingChild("All", Colors.black),
+      MapFloatingChild("테이크아웃", Colors.red),
+      MapFloatingChild("감성", Colors.orange),
+      MapFloatingChild("프렌차이즈", Colors.green),
+      MapFloatingChild("카공", Colors.blue),
+      MapFloatingChild("테마", Colors.purple),
+      MapFloatingChild("무인", Colors.pink),
+    ];
+
     return SpeedDial(
       label: Text(
         labelText,
@@ -43,158 +82,7 @@ class _LabelChangeState extends State<LabelChange> {
       elevation: 2,
       // Adjust shadow for separation
       children: [
-        SpeedDialChild(
-          labelWidget: SizedBox(
-            width: 80,
-            height: 40,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 8.0),
-              alignment: Alignment.center,
-              child: Text(
-                "All",
-                style: TextStyle(
-                    fontFamily: 'Freesentation',
-                    fontWeight: FontWeight.w600,
-                    color: Colors.black,
-                    fontSize: 14.0),
-              ),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Colors.white),
-            ),
-          ),
-          backgroundColor: Colors.white,
-          onTap: () {
-            onLabelChange("All");
-            print("all");
-          },
-        ),
-        SpeedDialChild(
-          labelWidget: SizedBox(
-            width: 80,
-            height: 40,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 8.0),
-              alignment: Alignment.center, // 중앙 정렬
-              child: Text(
-                "테이크 아웃",
-                style: TextStyle(
-                  fontFamily: 'Freesentation',
-                  fontWeight: FontWeight.w600,
-                  color: Colors.red,
-                  fontSize: 14.0,
-                ),
-              ),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Colors.white),
-            ),
-          ),
-          backgroundColor: Colors.white,
-          onTap: () {
-            onLabelChange("테이크 아웃");
-          },
-        ),
-        SpeedDialChild(
-          labelWidget: SizedBox(
-            width: 80,
-            height: 40,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 8.0),
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Colors.white),
-              child: Text(
-                "감성",
-                style: TextStyle(
-                    fontFamily: 'Freesentation',
-                    fontWeight: FontWeight.w600,
-                    color: Colors.orange,
-                    fontSize: 14.0),
-              ),
-            ),
-          ),
-          backgroundColor: Colors.white,
-          onTap: () {
-            onLabelChange("감성");
-          },
-        ),
-        SpeedDialChild(
-          labelWidget: SizedBox(
-            width: 80,
-            height: 40,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 8.0),
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Colors.white),
-              child: Text(
-                "프렌차이즈",
-                style: TextStyle(
-                    fontFamily: 'Freesentation',
-                    fontWeight: FontWeight.w600,
-                    color: Colors.green,
-                    fontSize: 14.0),
-              ),
-            ),
-          ),
-          backgroundColor: Colors.white,
-          onTap: () {
-            onLabelChange("프렌차이즈");
-          },
-        ),
-        SpeedDialChild(
-          labelWidget: SizedBox(
-            width: 80,
-            height: 40,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 8.0),
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Colors.white),
-              child: Text(
-                "카공",
-                style: TextStyle(
-                    fontFamily: 'Freesentation',
-                    fontWeight: FontWeight.w600,
-                    color: Colors.blue,
-                    fontSize: 14.0),
-              ),
-            ),
-          ),
-          backgroundColor: Colors.white,
-          onTap: () {
-            onLabelChange("카공");
-          },
-        ),
-        SpeedDialChild(
-          labelWidget: SizedBox(
-            width: 80,
-            height: 40,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 8.0),
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Colors.white),
-              child: Text(
-                "테마",
-                style: TextStyle(
-                    fontFamily: 'Freesentation',
-                    fontWeight: FontWeight.w600,
-                    color: Colors.purple,
-                    fontSize: 14.0),
-              ),
-            ),
-          ),
-          backgroundColor: Colors.white,
-          onTap: () {
-            onLabelChange("카공");
-          },
-        ),
+        ...buttons
       ],
     );
   }
@@ -204,3 +92,4 @@ class _LabelChangeState extends State<LabelChange> {
     return floatingButtons();
   }
 }
+
